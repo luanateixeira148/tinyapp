df47a113647d0ffe499b87bb0d4e51764ee02791
Luana <luanateixeira148@gmail.com>
2021-07-06 02:17:35 +0000

    added shortenURL function

--- express_server.js
+++ express_server.js
@@ -4,11 +4,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   "olm076" : "http://www.something.com"
 };
+
+const generateRandomString = function(){
+  return Math.random().toString(20).substr(2, 6)
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,10 +34,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const tempShortURL = req.params.shortURL;
   const templateVars = { shortURL: tempShortURL, longURL: urlDatabase[tempShortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
