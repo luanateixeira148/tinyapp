d1799419d9aef79cd9ac094e40db3fd19d81816e
Luana <luanateixeira148@gmail.com>
2021-07-08 15:03:26 +0000

    setup cookies

--- express_server.js
+++ express_server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -33,8 +35,21 @@ app.get("/urls.json", (req, res) => {
 });
 
 // Passing URL data to the template
+// app.get("/urls", (req, res) => {
+//   const cookieUsername = req.cookies.username;
+//   const templateVars = { urls: urlDatabase, username: cookieUsername };
+  
+//   console.log(templateVars);
+  
+//   res.render("urls_index", templateVars);
+// });
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies.username 
+  };
+  
   res.render("urls_index", templateVars);
 });
 
@@ -74,15 +89,21 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect('/urls');
 });
 
-
 // Edit the long url
 app.post('/urls/:shortURL', (req, res) => {
   const id = req.params.shortURL;
   const body = req.body[id];
   urlDatabase[id] = body;
-
   res.redirect('/urls');
 })
+
+// accepts the login form
+app.post('/login', (req, res) => {
+const username = req.body.username;
+res.cookie('username', username);
+
+res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
