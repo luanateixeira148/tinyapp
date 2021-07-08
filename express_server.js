de3358bff7025d2dd03ba79d37c0119f24df0196
Luana <luanateixeira148@gmail.com>
2021-07-08 05:12:55 +0000

    refactored code and added comments for clarity

--- express_server.js
+++ express_server.js
@@ -1,84 +1,88 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-  "olm076": "http://www.something.com"
+  "ozv250": "http://www.something.com"
 };
 
-//generates random string to be used as the shortURL
+// //generates random string to be used as the shortURL
 const generateRandomString = function(){
   return Math.random().toString(20).substr(2, 6)
 }
 
+// Just for demo purpose? - Handler to the root path
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// Just for demo purpose? - Handler to hello page
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+// Passing URL data to the template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Render the page with the new URL form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Render the individual URL page
 app.get("/urls/:shortURL", (req, res) => {
   const tempShortURL = req.params.shortURL;
   const templateVars = { shortURL: tempShortURL, longURL: urlDatabase[tempShortURL] };
   res.render("urls_show", templateVars);
 });
 
+// handles the new URL form submission
 app.post("/urls", (req, res) => {
-  let newLongURL = req.body.longURL;
-  let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = newLongURL;
-  res.redirect(`/urls/${newShortURL}`);
+  const newShortUrl = generateRandomString();
+  const newLongUrl = req.body.longURL;
+
+  urlDatabase[newShortUrl] = newLongUrl;
+
+  res.redirect(`/urls/${newShortUrl}`);
 });
 
+// redirect users to the long url if they click the short url
 app.get("/u/:shortURL", (req, res) => {
-  const tempLongURL = urlDatabase[req.params.shortURL];
-  console.log(tempLongURL);
-  res.redirect(tempLongURL);
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 
-// Edit POST /urls/:shortURL
+// deletes a url from the urls_index page
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+});
+
+
+// Edit the long url
 app.post('/urls/:shortURL', (req, res) => {
   const id = req.params.shortURL;
   const body = req.body[id];
-  
-  // console.log(id);
   urlDatabase[id] = body;
 
   res.redirect('/urls');
 })
-
-
-// Delete POST /urls/:shortURL/delete
-app.post('/urls/:shortURL/delete', (req, res) => {
-  const deleteShortURL = req.params.shortURL;
-  delete urlDatabase[deleteShortURL];
-
-  res.redirect('/urls');
-})
-
-/////
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
